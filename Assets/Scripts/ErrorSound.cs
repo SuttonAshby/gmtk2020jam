5c48e7460c183d4c7dd1e12ea1c5cd7a080df710
asbohaska <asbohaska@gmail.com>
2020-07-11 14:14:14 -0400

    morematerialsandsound

---
+++ Assets/Scripts/ErrorSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+  public class ErrorSound : MonoBehaviour
+{
+    public AudioClip error;
+	void Start ()
+	{
+		GetComponent<AudioSource> () .clip= error;
+	}
+
+	void OnCollisionEnter ()
+	{
+		GetComponent<AudioSource> () .Play ();
+	}
+}
