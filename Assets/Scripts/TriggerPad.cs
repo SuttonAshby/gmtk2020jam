036da96cb0e7a9b3a4dca78d3248df87421f7702
Sutton Ashby <sutton.ashby@gmail.com>
2020-07-11 15:33:05 -0400

    added triggerPad

---
+++ Assets/Scripts/TriggerPad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPad : MonoBehaviour
+{
+    public bool inTrigger = false;
+    public Renderer rend;
+    Color defaultColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = rend.material.color;
+    }
+
+    void OnTriggerEnter(){
+        inTrigger = true;
+    }
+
+    void OnTriggerStay(){
+
+    }
+
+    void OnTriggerExit(){
+        inTrigger = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inTrigger){
+            Color color = rend.material.color;
+                color.r = 0f; 
+                color.g = 1f; //green
+                color.b = 0f;
+                color.a = 1f;
+                rend.material.color = color;
+                rend.material.SetColor("_EmissionColor", color);
+        } else {
+            rend.material.color = defaultColor;
+            rend.material.SetColor("_EmissionColor", defaultColor);
+        }
+    }
+}
