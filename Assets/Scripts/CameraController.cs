c7bd44ee715b33253fd87e81e8ee5ce47fb908bb
Sutton Ashby <sutton.ashby@gmail.com>
2020-07-11 23:51:08 -0400

    fixed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     // public float speed = 3f;
 
     public float lookSpeed = 3f;
-    private Vector2 rotation = Vector2.zero;
+    private Vector3 rotation = Vector3.zero;
 
     public void Start() {
         Camera.main.transform.localRotation = Quaternion.Euler(120f, 0, 0);
@@ -19,7 +19,8 @@ public class CameraController : MonoBehaviour
         rotation.y += Input.GetAxis("Mouse X");
         rotation.x += -Input.GetAxis("Mouse Y");
         rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
-        transform.eulerAngles = new Vector2(0,rotation.y) * lookSpeed;
+        rotation.z = EventManager.Instance.cameraAngle;
+        transform.eulerAngles = new Vector3(0,rotation.y, rotation.z) * lookSpeed;
         Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
     }
 
--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -126,15 +126,17 @@ public class EventManager : MonoBehaviour
         }  
     }
 
-    public GameObject cameraObject;
+    // public GameObject cameraObject;
+    public float cameraAngle = 0f;
     //needs to be fixed with camera controller
     void changeCamera(bool setDefault){
         if(setDefault){
-            cameraObject.transform.Rotate(0,0,0);
+            cameraAngle = 0f;
         } else {
-            int[] cameraVals = {0, 90, 180, 270};
+            float[] cameraVals = {0f, 90f, 180f, 270f};
             var newVal = Random.Range(0, cameraVals.Length);
-            cameraObject.transform.Rotate(0, 0, cameraVals[newVal]);
+            // cameraObject.transform.Rotate(0, 0, cameraVals[newVal]);
+            cameraAngle = cameraVals[newVal];
             Debug.Log("Camera" + cameraVals[newVal]); 
         }
     }
